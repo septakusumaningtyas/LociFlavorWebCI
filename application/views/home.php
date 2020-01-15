bb8b21da65de44a188b63eb139bc0e6afdee60a8
Hesti <hestianisa94@gmail.com>
2020-01-15 12:17:09 +0700

    Link

--- application/views/home.php
+++ application/views/home.php
@@ -7,7 +7,7 @@
 
             <div class="col-md-12 col-sm-12 text-center ftco-animate">
               <h1 class="mb-4 mt-5">Our Delicious Specialties</h1>
-              <p><a href="reservation.html" class="btn btn-primary p-3 px-xl-4 py-xl-3">Order Now</a> <a href="menu.html" class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">View Menu</a></p>
+              <p><a href="<?php echo base_url("reservation"); ?>" class="btn btn-primary p-3 px-xl-4 py-xl-3">Order Now</a> <a href="<?php echo base_url("menu"); ?>" class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">View Menu</a></p>
             </div>
 
           </div>
@@ -21,7 +21,7 @@
 
             <div class="col-md-12 col-sm-12 text-center ftco-animate">
               <h1 class="mb-4 mt-5">The Best Place to Kick of Your Day</h1>
-              <p><a href="reservation.html" class="btn btn-primary p-3 px-xl-4 py-xl-3">Order Now</a> <a href="menu.html" class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">View Menu</a></p>
+              <p><a href="<?php echo base_url("reservation"); ?>" class="btn btn-primary p-3 px-xl-4 py-xl-3">Order Now</a> <a href="<?php echo base_url("menu"); ?>" class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">View Menu</a></p>
             </div>
 
           </div>
@@ -35,7 +35,7 @@
 
             <div class="col-md-12 col-sm-12 text-center ftco-animate">
               <h1 class="mb-4 mt-5">Creamy Hot and Ready to Serve</h1>
-              <p><a href="reservation.html" class="btn btn-primary p-3 px-xl-4 py-xl-3">Order Now</a> <a href="menu.html" class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">View Menu</a></p>
+              <p><a href="<?php echo base_url("reservation"); ?>" class="btn btn-primary p-3 px-xl-4 py-xl-3">Order Now</a> <a href="<?php echo base_url("menu"); ?>" class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">View Menu</a></p>
             </div>
 
           </div>
