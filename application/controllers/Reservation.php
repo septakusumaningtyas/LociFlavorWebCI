2fd4daa013af1c840e0ce1ab4d2ad9e667d2db2e
septakusumaningtyas <septakusumaningtyas@gmail.com>
2020-01-23 11:42:20 +0700

    Reservation --

--- application/controllers/Reservation.php
+++ application/controllers/Reservation.php
@@ -5,13 +5,26 @@ class Reservation extends CI_Controller {
    public function index()
    {
       $this->load->model('model_reservation');
-      //$data['reservation'] = $this->model_reservation->list_reservation()->result();
+      //$data['reservation'] = $this->model_reservation->GetReservation()->result();
       $data['title'] = "Loci Flavor - Reservation";
       $this->load->view('template/header' , $data);
       $this->load->view('template/navbar');
       $this->load->view('reservation');
-      //$this->load->view('list_reservation',$data);
+      $this->load->view('list_reservation',$data);
       $this->load->view('template/footer');
       $this->load->view('template/script');
+   }
+   function input()
+   {
+     $this->load->view('reservation');
+   }
+    
+   function input_simpan()
+   {
+      $data = array(
+         'kode_barang'   =>  $this->input->post('kode_barang'),
+         'nama_barang'   =>  $this->input->post('nama_barang'),            'harga'         =>  $this->input->post('harga_barang'));
+        $this->db->insert('tb_reservation',$data);
+        redirect('tb_reservation');   
    }
 }
